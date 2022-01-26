80e1aaff42cae587b0d1ebcb495e0da7a6970c6f
danfortunato <dan.fortunato@gmail.com>
2022-01-25 19:03:08 -0500

    Remove old array implementation

--- @treefun2_arrays/height.m
+++
@@ -1,7 +0,0 @@
-function out = height(f)
-%HEIGHT   Length of a TREEFUN2.
-%   HEIGHT(F) returns the number of levels in the TREEFUN2.
-
-out = max(f.height) + 1;
-
-end
--- @treefun2_arrays/private/isLeaf.m
+++
@@ -1,6 +0,0 @@
-function out = isLeaf(f, id)
-%ISLEAF   Is this box a leaf?
-
-out = ( f.height(id) == 0 );
-
-end
