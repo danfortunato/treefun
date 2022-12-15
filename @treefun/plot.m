95f6061fb71e7720331b9445eb7395c98ffbe50c
danfortunato <dan.fortunato@gmail.com>
2022-12-14 19:03:59 -0500

    Support complex values in treefun/plot

--- @treefun/plot.m
+++ @treefun/plot.m
@@ -3,7 +3,7 @@ function varargout = plot(f, varargin)
 %   PLOT(F) plots the TREEFUN F and shows the tree on which F is defined.
 
 holdState = ishold();
-nplotpts = 200;
+nplotpts = 100;
 ms = 10;
 lw = 1;
 
@@ -14,15 +14,29 @@ for k = 1:length(boxes)
     box = boxes(k);
     x = linspace(box.domain(1), box.domain(2), nplotpts);
     y = coeffs2plotvals(box.coeffs);
-    hk = plot(x, y, 'LineWidth', lw, varargin{:});
+    if ( isreal(y) )
+        hk = plot(x, y, 'LineWidth', lw, varargin{:});
+    else
+        hk = plot(y, 'LineWidth', lw, varargin{:});
+    end
     if ( nargout > 0 )
         h(k) = hk; %#ok<AGROW>
     end
-    plot(boxes(k).domain(1), y(1), 'k.', 'MarkerSize', ms)
+    if ( isreal(y) )
+        plot(boxes(k).domain(1), y(1), 'k.', 'MarkerSize', ms)
+    else
+        plot(y(1), 'k.', 'MarkerSize', ms)
+    end
 end
-plot(boxes(end).domain(2), y(end), 'k.', 'MarkerSize', ms)
+if ( isreal(y) )
+    plot(boxes(end).domain(2), y(end), 'k.', 'MarkerSize', ms)
+else
+    plot(y(end), 'k.', 'MarkerSize', ms)
+end
 
-xlim(f.domain)
+if ( isreal(y) )
+    xlim(f.domain)
+end
 
 if ( ~holdState )
     hold off
