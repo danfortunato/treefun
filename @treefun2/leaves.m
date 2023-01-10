d1bb4be82bb0bf56c5b2ad71890fb2e997a2100b
danfortunato <dan.fortunato@gmail.com>
2023-01-09 19:45:30 -0500

    Optimize leaves()

--- @treefun2/leaves.m
+++ @treefun2/leaves.m
@@ -2,11 +2,6 @@ function ids = leaves(f)
 %LEAVES   Get the leaf IDs in a TREEFUN2.
 %   LEAVES(F) returns the leaf IDs in the TREEFUN2 F.
 
-ids = [];
-for k = 1:length(f.id)
-    if ( f.height(k) == 0 )
-        ids(end+1) = f.id(k); %#ok<AGROW>
-    end
-end
+ids = f.id(f.height == 0);
 
 end
