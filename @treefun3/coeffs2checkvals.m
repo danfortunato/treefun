2374be08223bf34333c65491d1663067d182385c
Hai Zhu <zhuh1106@gmail.com>
2024-02-10 22:46:15 -0500

    update tree plot routine

---
+++ @treefun3/coeffs2checkvals.m
@@ -0,0 +1,28 @@
+function vals = coeffs2checkvals(coeffs,x,y,z)
+%
+%
+
+persistent Evalx Evaly Evalz
+p = size(coeffs, 1);
+ncheckpts = numel(x); % hopefully not too large...
+
+Evalx = ones(ncheckpts, p);
+Evaly = ones(ncheckpts, p);
+Evalz = ones(ncheckpts, p);
+Evalx(:,2) = x(:);
+Evaly(:,2) = y(:);
+Evalz(:,2) = z(:);
+for k=3:p
+  Evalx(:,k) = 2*x(:).*Evalx(:,k-1)-Evalx(:,k-2);
+  Evaly(:,k) = 2*y(:).*Evaly(:,k-1)-Evaly(:,k-2);
+  Evalz(:,k) = 2*z(:).*Evalz(:,k-1)-Evalz(:,k-2);
+end
+% coeffs to value map
+vals = zeros(ncheckpts,1);
+for k=1:ncheckpts
+  tmp1 = permute(tensorprod(Evalx(k,:),coeffs,2,1),[2 3 1]);
+  tmp2 = permute(tensorprod(Evaly(k,:),tmp1,2,1),[2 3 1]);
+  vals(k) = permute(tensorprod(Evalz(k,:),tmp2,2,1),[2 3 1]);
+end
+
+end
